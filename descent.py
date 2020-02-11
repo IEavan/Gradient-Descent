4df48b8fdcab2023a439fdc6ca38e05ce933a6dc
Eavan Thomas Pattie <eavan.pattie@gmail.com>
2020-02-11 18:50:42 +0000

    Added vanilla gradient descent on iris data

--- descent.py
+++ descent.py
@@ -1,9 +1,12 @@
 import torch
 import itertools
+import functools
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import animation
+
 
 # Task 1.1
-
-
 def gd_back(x_start, loss_func, alpha=0.5, beta=0.5, s=1):
     """ Compute minimum using gradient descent with backtracking """
     x = x_start
@@ -26,7 +29,7 @@ def gd_back(x_start, loss_func, alpha=0.5, beta=0.5, s=1):
             grad.copy_(x.grad)
             x = x - eta * x.grad
 
-            yield x, grad
+        yield x, grad
 
 
 def get_11_loss(x_size):
@@ -73,3 +76,61 @@ print("Min x^2 + y^2 / 100")
 xs = gd_back(torch.Tensor([0.01, 1]), lambda x: x[0].pow(2) + 0.01 * x[1].pow(2),
              alpha=0.25, beta=0.5, s=2)
 optim(xs, limit=10 ** -5)
+
+
+# Task 2
+def cost_function(X, labels, w, epsilon=1e-8):
+    """ Compute the average cross entropy loss """
+    scores = torch.mv(X, w).sigmoid()
+    pos = - torch.log(scores + epsilon)
+    neg = - torch.log(1 - scores - epsilon)
+    return (labels * pos + (1 - labels) * neg).mean()
+
+
+def gradient_descent(x_start, loss_func, eta):
+    x = x_start
+    while True:
+        x.requires_grad = True
+        loss = loss_func(x)
+        loss.backward()
+
+        with torch.no_grad():
+            x = x - eta * x.grad
+
+        yield x
+
+
+data = np.loadtxt("newiris.csv", delimiter=",")
+X = np.c_[data[:, 2:-1], np.ones(data.shape[0])]
+labels = data[:, -1]
+loss_function = functools.partial(cost_function,
+                                  torch.Tensor(X), torch.Tensor(labels))
+w_start = torch.zeros(3)
+ws = gradient_descent(w_start, loss_function, 1)
+trace = []
+print("Finding decision boundary for iris data")
+for i, w in enumerate(ws):
+    if i % 20 == 0:
+        trace.append(w.numpy())
+    if i % 1000 == 0:
+        print(f"Iteration {i} | Loss: {loss_function(w)}")
+        print(f"Current W = {w}")
+    if i > 10000:
+        break
+
+fig = plt.figure()
+ax = plt.axes(ylim=(0.5, 3))
+ax.scatter(X[:50, 0], X[:50, 1], c='g')
+ax.scatter(X[51:, 0], X[51:, 1], c='b')
+line, = ax.plot([], [])
+
+
+def plot_frame(w):
+    y_data = [-1, 3]  # From axis size
+    x_data = [(-w[1] * y - w[2])/w[0] for y in y_data]
+    line.set_data(x_data, y_data)
+    return line,
+
+
+ani = animation.FuncAnimation(fig, plot_frame, trace, blit=True)
+plt.show()
